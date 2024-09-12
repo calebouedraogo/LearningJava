48adaf19742d28b7c7deeb255447e1053ed869b3
CALEB OUEDRAOGO <calebouedraogo@calebs-mbp.mynetworksettings.com>
2024-09-12 10:20:10 -0400

    Added repeatSong class

---
+++ LeaningJava/src/main/java/repeatSong.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class repeatSong {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean isOnRepeat = true;
+        while (isOnRepeat) {
+            System.out.println("Playing current song");
+            System.out.println("Would you like to yake this song off of repeat? If so, answer yes");
+            String userInput = input.next();
+
+            if (userInput.equals("yes")) {
+                isOnRepeat = false;
+            }
+        }
+        System.out.println("Playing the next song");
+    }
+}
